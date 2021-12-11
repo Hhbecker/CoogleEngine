a20e28b3a3efb136e0a1e6a395315cb5c7c7f1b4
Hhbecker <Hbeckerhenry@gmail.com>
2021-12-11 16:53:56 -0500

    read_query, rank

--- hashmap.h
+++ hashmap.h
@@ -5,14 +5,15 @@
 // defines document linked list struct
 struct docNode {
         int docID;
-        int numOccurences;
+        int termFrequency;
+        double* tfidf;
         struct docNode* nextDoc; 
 };
 
 // defines word linked list struct
 struct wordNode {
         char* word;
-        int df;
+        int docFrequency;
         struct wordNode* nextWord;
         struct docNode* docList;
 };
@@ -30,11 +31,15 @@ struct hashmap* training(char* directory, char* charBuckets );
 void hash_table_insert(struct hashmap* hashmapPtr, char* word, int docID);
 struct wordNode* findWord(struct hashmap* hm, int index, char* word, int insertMode);
 void addDoc(struct wordNode* wordPtr, int docID);
+void stop_word(struct hashmap* hm, int numDocs);
 struct hashmap* hm_create(int num_buckets);
 int hm_get(struct hashmap* hm, char* word, char* document_id);
 void hm_put(struct hashmap* hm, char* word, char* document_id, int num_occurrences);
-void hm_remove(struct hashmap* hm, char* word, char* document_id);
+void removeWord(struct hashmap* hm, struct wordNode* trailingPtr, struct wordNode* Ptr);
+void freeWord(struct wordNode* wordPtr);
 void hm_destroy(struct hashmap* hm);
-int hash(struct hashmap* hm, char* word, int document_id);
+int hash(int numBuckets, char* word);
+void rank(struct hashmap* hm, char** query);
+char** read_query(void);
 
 #endif
